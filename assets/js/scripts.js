fa4971a31739781ff9e784c9b815573c23c45a3f
= <=>
2022-06-15 12:55:51 +0500

    location form styling done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
 
     // animation on scroll
@@ -314,28 +312,73 @@ $(document).ready(function () {
     }
 
     if (window.File && window.FileList && window.FileReader) {
-        $("#files").on("change", function(e) {
-          var files = e.target.files,
-            filesLength = files.length;
-          for (var i = 0; i < filesLength; i++) {
-            var f = files[i]
-            var fileReader = new FileReader();
-            fileReader.onload = (function(e) {
-              var file = e.target;
-              $("<span class=\"pip\">" +
-                "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
-                "<br/><span class=\"remove\"><i class='fa-solid fa-circle-xmark'></i></span>" +
-                "</span>").insertAfter("#files");
-              $(".remove").click(function(){
-                $(this).parent(".pip").remove();
-              });
-            });
-            fileReader.readAsDataURL(f);
-          }
+        $("#files").on("change", function (e) {
+            var files = e.target.files,
+                filesLength = files.length;
+            for (var i = 0; i < filesLength; i++) {
+                var f = files[i]
+                var fileReader = new FileReader();
+                fileReader.onload = (function (e) {
+                    var file = e.target;
+                    $("<span class=\"pip\">" +
+                        "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
+                        "<br/><span class=\"remove\"><i class='fa-solid fa-circle-xmark'></i></span>" +
+                        "</span>").insertAfter("#files");
+                    $(".remove").click(function () {
+                        $(this).parent(".pip").remove();
+                    });
+                });
+                fileReader.readAsDataURL(f);
+            }
         });
-      } else {
+    } else {
         alert("Your browser doesn't support to File API")
-      }
+    }
+
+    $('#files').change(function() {
+        $(this).addClass('active');
+    })
+
+    $('#branch_name').keyup(function () {
+        if ($(this).val().length > 1 && $('#description').val().length > 1 && $('#google_map').val().length > 1 && $('#website').val().length > 1 && $('#contact').val().length === 13 && $('#files').hasClass('active')) {
+            $('#location-btn').removeClass('disabled').addClass('active');
+        }
+        else {
+            $('#location-btn').addClass('disabled').removeClass('active');
+        }
+    })
+    $('#description').keyup(function () {
+        if ($(this).val().length > 1 && $('#branch_name').val().length > 1 && $('#google_map').val().length > 1 && $('#website').val().length > 1 && $('#contact').val().length === 13 && $('#files').hasClass('active')) {
+            $('#location-btn').removeClass('disabled').addClass('active');
+        }
+        else {
+            $('#location-btn').addClass('disabled').removeClass('active');
+        }
+    })
+    $('#google_map').keyup(function () {
+        if ($(this).val().length > 1 && $('#branch_name').val().length > 1 && $('#description').val().length > 1 && $('#website').val().length > 1 && $('#contact').val().length === 13 && $('#files').hasClass('active')) {
+            $('#location-btn').removeClass('disabled').addClass('active');
+        }
+        else {
+            $('#location-btn').addClass('disabled').removeClass('active');
+        }
+    })
+    $('#website').keyup(function () {
+        if ($(this).val().length > 1 && $('#branch_name').val().length > 1 && $('#description').val().length > 1 && $('#google_map').val().length > 1 && $('#contact').val().length === 13 && $('#files').hasClass('active')) {
+            $('#location-btn').removeClass('disabled').addClass('active');
+        }
+        else {
+            $('#location-btn').addClass('disabled').removeClass('active');
+        }
+    })
+    $('#contact').keyup(function () {
+        if ($(this).val().length === 13 && $('#branch_name').val().length > 1 && $('#description').val().length > 1 && $('#google_map').val().length > 1 && $('#website').val().length > 1 && $('#files').hasClass('active')) {
+            $('#location-btn').removeClass('disabled').addClass('active');
+        }
+        else {
+            $('#location-btn').addClass('disabled').removeClass('active');
+        }
+    })
 
 });
 
